78c32071f21f9763a2056acd86897a78bf9188e8
amberlinda <aul123linda@gmail.com>
2022-04-23 00:43:08 +0530

    login page added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React from 'react';
+import { Route,Link,Routes } from 'react-router-dom'
 import LandingPage from './components/landingPage/landingPage'
+import Login from './components/Login/login'
 
 function App() {
+
+  const routes = [{
+    path:"/",
+    element:<LandingPage/>
+  },{
+    path:"/login",
+    element:<Login/>
+  }]
+
   return (
-    <LandingPage/>
+    <Routes>
+      {routes.map(el => <Route path={el.path} element={el.element}/>)}
+    </Routes>
   );
 }
 
--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import logo from '../../img/theatex_logo.png'
+
+import css from './login.module.css'
 
 const Login = () => {
     return(
-        <div>
-            
+        <div className={css.login_cont}>
+            <div className={css.left_cont}>
+                <div className={css.left_child_cont}>
+                    <img src={logo}/>
+                    <p>Login to your account and gain access to all the features</p>
+                </div>
+            </div>
+            <div className={css.right_cont}>
+                <div className={css.right_child_cont}>
+                    <h3>Login</h3>
+                    <form>
+                        <div className={css.form_element_cont}>
+                            <label>Email</label>
+                            <input type="email" placeholder='email'/>
+                        </div>
+                        <div className={css.form_element_cont}>
+                            <label>Password</label>
+                            <input type="password" placeholder='password'/>
+                        </div>
+                        <button className={css.submitBtn} type='submit'>Login now</button>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 }
